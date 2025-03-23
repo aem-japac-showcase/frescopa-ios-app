9b6e69819ca411f5d7dc55132538386b69c4a3a9
Mark Szulc <mszulc@adobe.com>
2025-03-23 22:00:46 +1100

    Initial Commit

---
+++ frescopa/frescopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  frescopaApp.swift
+//  frescopa
+//
+//  Created by Mark Szulc on 23/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct frescopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
